96d6fc89cde28e3ca67e700cb0521178af744175
juayala <143744588+juayala@users.noreply.github.com>
2025-05-05 05:27:35 +0000

    Refactor SQL model for daily website sessions to improve readability and formatting

--- dbt_basket_craft/models/warehouse/fct_website_sessions_utm_source_daily.sql
+++ dbt_basket_craft/models/warehouse/fct_website_sessions_utm_source_daily.sql
@@ -1,10 +1,19 @@
+WITH website_sessions AS (
+  SELECT
+    website_session_id,
+    website_session_created_at,
+    is_repeat_session,
+    utm_source
+  FROM {{ ref('stg_website_sessions') }}
+)
+
 SELECT
   DATE(website_session_created_at) AS website_session_day,
   utm_source,
   COUNT(website_session_id) AS sessions,
   SUM(is_repeat_session)::INT AS repeat_sessions,
   ROUND(
-    (SUM(is_repeat_session)::NUMERIC / COUNT(website_session_id)) * 100, 
+    (SUM(is_repeat_session)::NUMERIC / COUNT(website_session_id)) * 100,
     2
   ) AS repeat_sessions_pct
 FROM website_sessions
